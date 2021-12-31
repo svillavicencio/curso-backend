63ea7331107590d59babc409c4c0727c4393345c
Sergio Villavicencio <sergionicolasvillavicencio@gmail.com>
2021-12-31 01:43:00 -0300

    actualizado desafio3

--- desafio3/index.ts
+++ desafio3/index.ts
@@ -2,82 +2,74 @@ import fs from 'fs';
 
 class Container {
     constructor(public itemsList: string, private items: Array<object> = []){
-        
+        this.itemsList = `./${itemsList}`;
+        this.items;
+    }
 
-        fs.readFile(`./${itemsList}`, 'utf-8', (err, response) => {
+    public async init(){
+        let response = await fs.promises.readFile(this.itemsList, 'utf-8');
+        let data = JSON.parse(response);
+        for (let item of data){
+            let id = Container.id;
+            Container.id++;
 
-            if(err) throw err;
+            let newItemID = {
+                ...item,
+                id
+            }
 
-            this.items = JSON.parse(response);
-
-            this.items.forEach( (item , index) => {
-                let newItem = {id: Container.id, ...item}
-                Container.id++;
-                this.items[index] = newItem;
-            })
-            this.write(`./${this.itemsList}`, this.items);
-
-            
-        })
-
+            this.items.push(newItemID);
+            await this.write();
+        }
     }
 
     private static id = 1;
 
-    save(item: object){
-        let result = Container.id
-        this.read(`./${this.itemsList}`).then(()=>{
-            
-            let newItem = {id: Container.id, ...item}
-            Container.id++;
-            
-            this.items.push(newItem);
-            console.log('nuevo', this.items);
-            this.write(`./${this.itemsList}`, this.items);
-        }
-        )
-        
+    async save(item: object){
+        await this.read()
+
+        let newItem = { 
+            ...item,
+            id: Container.id}
+
+        let result = Container.id;
+        Container.id++;
+        this.items.push(newItem);
+
+        await this.write();
 
         return result;
 
     }
 
-    getById(id: number) {
-        this.read(`./${this.itemsList}`).then(()=>{
-            let item: {[id: string]: any};
-            for(item of this.items){
-                if(item.id === id){
-                    return item;
-                }
-                 
-            }
-    
-            return null;
-        })
+    async getById(id: number) {
+        await this.read()
+        return this.items[id-1] || null;
     }
 
-    getAll() {
-        this.read(`./${this.itemsList}`).then(()=>{return this.items})
-        
+    async getAll() {
+        await this.read();
+        return this.items;
     }
 
-    deleteById(id: number){
-        this.read(`./${this.itemsList}`).then(()=>{
-            this.items.splice(id, 1);
-            this.write(`./${this.itemsList}`, this.items);
-        })
-        
+    async deleteById(id: number){
+        await this.read()
+        this.items.splice(id - 1, 1);
+        this.reIndex();
+        await this.write()
+        return 'Elemento eliminado';
     }
 
     async deleteAll(){
-        this.write(`./${this.itemsList}`, []);
-        console.log(this.items)
+        this.items = [];
+        await this.write();
+        return 'Base de datos formateado';
 
     }
 
-    private async write(route: string, items: Array<object>){
+    private async write(){
         try{
-            await fs.promises.writeFile(route, JSON.stringify(items))
+            await fs.promises.writeFile(this.itemsList, JSON.stringify(this.items))
             console.log('guardado')
             console.log(this.items)
         }catch(err){
@@ -85,17 +77,36 @@ class Container {
         }
     }
 
-    private async read(route:string){
+    private async read(){
         try{
-            let response = await fs.promises.readFile(route, 'utf-8');
-            this.items = JSON.parse(response);
-            console.log('actualizado')
-            console.log(this.items)
+            let response = await fs.promises.readFile(this.itemsList, 'utf-8');
+            let result = JSON.parse(response)
+            this.items = result;
+            console.log('actualizado', this.items)
+            return result;
         }catch(err){
             throw err;
         }
     }
 
+    private reIndex(){
+
+        this.items.forEach((item: {[id: string]: any}, index) => {
+            item.id = index + 1;
+        })
+    }
+
 }
 
-let test = new Container('productos.txt')
+let test = new Container('productos.txt');
+test.init().then(async()=>{
+    // test.save( {
+    //     "title": "Globo Terráqueo",
+    //     "price": 0.67,
+    //     "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
+    //  })
+    test.getById(10).then((resp)=>{console.log(resp, 'getbyid')})
+    // test.getAll().then(resp=>console.log(resp,'getall'))
+    // test.deleteById(1)
+    test.deleteAll();
+})
