3b26b0da1c826059920322b403fab43854e8d323
Sergio Villavicencio <sergionicolasvillavicencio@gmail.com>
2022-01-01 18:15:36 -0300

    Se modifico delete y getbyid

--- desafio3/index.ts
+++ desafio3/index.ts
@@ -7,19 +7,24 @@ class Container {
     }
 
     public async init(){
-        let response = await fs.promises.readFile(this.itemsList, 'utf-8');
-        let data = JSON.parse(response);
-        for (let item of data){
-            let id = Container.id;
-            Container.id++;
-
-            let newItemID = {
-                ...item,
-                id
+        try {
+            let response = await fs.promises.readFile(this.itemsList, 'utf-8');
+            let data = JSON.parse(response);
+            for (let item of data){
+                let id = Container.id;
+                Container.id++;
+    
+                let newItemID = {
+                    ...item,
+                    id
+                }
+    
+                this.items.push(newItemID);
+                await this.write();
             }
-
-            this.items.push(newItemID);
-            await this.write();
+            return `Base de datos inicializado`
+        } catch (error) {
+            throw error;
         }
     }
 
@@ -43,8 +48,14 @@ class Container {
     }
 
     async getById(id: number) {
-        await this.read()
-        return this.items[id-1] || null;
+        try{
+            await this.read();
+            return this.items.find((item: {[id:string]: any})=>{
+                return item.id === id;
+            }) || null;
+        }catch(err){
+            throw err;
+        }
     }
 
     async getAll() {
@@ -53,10 +64,15 @@ class Container {
     }
 
     async deleteById(id: number){
-        await this.read()
-        this.items.splice(id - 1, 1);
-        this.reIndex();
-        await this.write()
+        try{
+            await this.read();
+            this.items = this.items.filter((item: {[id:string]: any})=>{
+                return item.id !== id;
+            })
+            await this.write();
+        }catch(err){
+            throw err;
+        }
         return 'Elemento eliminado';
     }
 
@@ -70,8 +86,6 @@ class Container {
     private async write(){
         try{
             await fs.promises.writeFile(this.itemsList, JSON.stringify(this.items))
-            console.log('guardado')
-            console.log(this.items)
         }catch(err){
             throw err;
         }
@@ -82,31 +96,41 @@ class Container {
             let response = await fs.promises.readFile(this.itemsList, 'utf-8');
             let result = JSON.parse(response)
             this.items = result;
-            console.log('actualizado', this.items)
             return result;
         }catch(err){
             throw err;
         }
     }
 
-    private reIndex(){
+    // private reIndex(){
 
-        this.items.forEach((item: {[id: string]: any}, index) => {
-            item.id = index + 1;
-        })
-    }
+    //     this.items.forEach((item: {[id: string]: any}, index) => {
+    //         item.id = index + 1;
+    //     })
+    // }
 
 }
 
 let test = new Container('productos.txt');
-test.init().then(async()=>{
-    // test.save( {
-    //     "title": "Globo Terráqueo",
-    //     "price": 0.67,
-    //     "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
-    //  })
-    test.getById(10).then((resp)=>{console.log(resp, 'getbyid')})
-    // test.getAll().then(resp=>console.log(resp,'getall'))
-    // test.deleteById(1)
-    test.deleteAll();
-})
+async function initDb() {
+    try{
+        console.log('Se cargan los items del archivo', await test.init(), await test.getAll());
+        console.log(
+        'Test save()',
+        await test.save( {
+            "title": "Globo Terráqueo",
+            "price": 0.67,
+            "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
+         }),
+        await test.getAll(), 
+        );
+        console.log('Test getById()', await test.getById(2))
+        console.log('Test deleteById()',await test.deleteById(2), await test.getAll());
+        console.log('Test deleteAll()',await test.deleteAll() ,await test.getAll());
+
+    }catch(err){
+        throw err;
+    }
+}
+
+initDb();
